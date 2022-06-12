e843f875c7a793223d4e5b24e622770a9f60b87f
Daniel Stack <22675319+kitescandal@users.noreply.github.com>
2022-06-12 07:46:57 -0400

    Fixed indenting

--- usTimer.h
+++ usTimer.h
@@ -7,45 +7,45 @@
 
 class usTimer
 {
-	LARGE_INTEGER startCount;
-	LARGE_INTEGER endCount;
-	LARGE_INTEGER pauseCount;
-	LARGE_INTEGER freq;
+    LARGE_INTEGER startCount;
+    LARGE_INTEGER endCount;
+    LARGE_INTEGER pauseCount;
+    LARGE_INTEGER freq;
 
 public:
-	usTimer() {
-	    QueryPerformanceFrequency(&freq);
-	}
-
-	void start()
-	{
-	    endCount.QuadPart = 0;
-	    pauseCount.QuadPart = 0;
-	    QueryPerformanceCounter(&startCount);
+    usTimer() {
+        QueryPerformanceFrequency(&freq);
     }
 
-	void stop() {
-	    QueryPerformanceCounter(&endCount);
+    void start()
+    {
+        endCount.QuadPart = 0;
+        pauseCount.QuadPart = 0;
+        QueryPerformanceCounter(&startCount);
+    }
+
+    void stop() {
+        QueryPerformanceCounter(&endCount);
     }
 
     void restart()
     {
-	    pauseCount.QuadPart += endCount.QuadPart - startCount.QuadPart;
+        pauseCount.QuadPart += endCount.QuadPart - startCount.QuadPart;
         endCount.QuadPart = 0;
-	    QueryPerformanceCounter(&startCount);
+        QueryPerformanceCounter(&startCount);
     }
 
-	int get()
-	{
-	    LARGE_INTEGER usedCount;
+    int get()
+    {
+        LARGE_INTEGER usedCount;
 
-	    if(endCount.QuadPart)
+        if(endCount.QuadPart)
             usedCount = endCount;
         else
             QueryPerformanceCounter(&usedCount);
 
-	    return (int)((usedCount.QuadPart - startCount.QuadPart + pauseCount.QuadPart) * 1000000 / freq.QuadPart);
-	}
+        return (int)((usedCount.QuadPart - startCount.QuadPart + pauseCount.QuadPart) * 1000000 / freq.QuadPart);
+    }
 };
 
 #endif // US_TIMER_H
